23c17a6923a2b15006a8c0e625fe67a6b8637476
Diego0Gam <diegogamarrra@ctpantoniomarte.edu.ar>
2025-06-09 16:37:36 -0300

    Especificaciones de clases en HTML y cambios en los links

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -9,28 +9,28 @@ app_name = "inventario"
 
 urlpatterns = [
 #Estudiante
-    path('crear/', EstudianteCreateView.as_view(), name = 'estudiante_create'),
-    path('lista/', EstudianteListView.as_view(), name = 'estudiante_list'),
-    path('<int:pk>/editar/', EstudianteUpdateView.as_view(), name = 'estudiante_update'),
-    path('<int:pk>/eliminar/', EstudianteDeleteView.as_view(), name = 'estudiante_delete'),
+    path('estudiante/crear/', EstudianteCreateView.as_view(), name = 'estudiante_create'),
+    path('estudiante/lista/', EstudianteListView.as_view(), name = 'estudiante_list'),
+    path('estudiante/<int:pk>/editar/', EstudianteUpdateView.as_view(), name = 'estudiante_update'),
+    path('estudiante/<int:pk>/eliminar/', EstudianteDeleteView.as_view(), name = 'estudiante_delete'),
 #Curso
     path('curso/crear/', CursoCreateView.as_view(), name = 'curso_create'),
     path('curso/lista/', CursoListView.as_view(), name = 'curso_list'),
     path('curso/<int:pk>/editar/', CursoUpdateView.as_view(), name = 'curso_update'),
     path('curso/<int:pk>/eliminar/', CursoDeleteView.as_view(), name = 'curso_delete'),
 #Materia
-    path('crear/', MateriaCreateView.as_view(), name = 'materia_create'),
-    path('lista/', MateriaListView.as_view(), name = 'materia_list'),
-    path('<int:pk>/editar/', MateriaUpdateView.as_view(), name = 'materia_update'),
-    path('<int:pk>/eliminar/', MateriaDeleteView.as_view(), name = 'materia_delete'),
+    path('materia/crear/', MateriaCreateView.as_view(), name = 'materia_create'),
+    path('materia/lista/', MateriaListView.as_view(), name = 'materia_list'),
+    path('materia/<int:pk>/editar/', MateriaUpdateView.as_view(), name = 'materia_update'),
+    path('materia/<int:pk>/eliminar/', MateriaDeleteView.as_view(), name = 'materia_delete'),
 #Computadora
-    path('crear/', ComputadoraCreateView.as_view(), name = 'computadora_create'),
-    path('lista/', ComputadoraListView.as_view(), name = 'computadora_list'),
-    path('<int:pk>/editar/', ComputadoraUpdateView.as_view(), name = 'computadora_update'),
-    path('<int:pk>/eliminar/', ComputadoraDeleteView.as_view(), name = 'computadora_delete'),
+    path('computadora/crear/', ComputadoraCreateView.as_view(), name = 'computadora_create'),
+    path('computadora/lista/', ComputadoraListView.as_view(), name = 'computadora_list'),
+    path('computadora/<int:pk>/editar/', ComputadoraUpdateView.as_view(), name = 'computadora_update'),
+    path('computadora/<int:pk>/eliminar/', ComputadoraDeleteView.as_view(), name = 'computadora_delete'),
 #Prestamo
-    path('crear/', PrestamoCreateView.as_view(), name = 'prestamo_create'),
-    path('lista/', PrestamoListView.as_view(), name = 'prestamo_list'),
-    path('<int:pk>/editar/', PrestamoUpdateView.as_view(), name = 'prestamo_update'),
-    path('<int:pk>/eliminar/', PrestamoDeleteView.as_view(), name = 'prestamo_delete')
+    path('prestamo/crear/', PrestamoCreateView.as_view(), name = 'prestamo_create'),
+    path('prestamo/lista/', PrestamoListView.as_view(), name = 'prestamo_list'),
+    path('prestamo/<int:pk>/editar/', PrestamoUpdateView.as_view(), name = 'prestamo_update'),
+    path('prestamo<int:pk>/eliminar/', PrestamoDeleteView.as_view(), name = 'prestamo_delete')
 ]
